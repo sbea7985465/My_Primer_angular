b84861407a7e0fd89b973e72f56d9a61bf232175
Sebastian Choque <sebastian.choque.ponce@cftmail.cl>
2024-10-13 23:15:50 -0300

    Tarea de trabajo de angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
+import { RegistroComponent } from './registro/registro.component';
+export const routes: Routes = [
+    {
+        path: "inicio",
 
-export const routes: Routes = [];
+        component: InicioComponent
+    },
+
+    {
+        path:"contactanos",
+        component: ContactanosComponent
+    },
+
+    {
+        path: "registro",
+        component: RegistroComponent
+    }
+
+
+];
